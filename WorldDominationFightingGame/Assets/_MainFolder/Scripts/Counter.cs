243c191aa7a172771eb1247d5a1209ad174fd982
Sebastian Burckhardt <sburckhardr98@gmail.com>
2023-01-04 11:54:23 +0100

    make counter interactive

--- WorldDominationFightingGame/Assets/_MainFolder/Scripts/Counter.cs
+++ WorldDominationFightingGame/Assets/_MainFolder/Scripts/Counter.cs
@@ -5,6 +5,7 @@ using TMPro;
 
 public class Counter : MonoBehaviour
 {
+
     public TriggerCounter p1Score;
     public TriggerCounter p2Score;
     public TriggerCounter p3Score;
@@ -19,9 +20,23 @@ public class Counter : MonoBehaviour
     public TextMeshProUGUI text2;
     public TextMeshProUGUI text3;
 
+    public RectTransform panel1;
+    public RectTransform panel2;
+    public RectTransform panel3;
+
+    public float maxWidth = 620;
+    public float minWidth = 250;
+    float fullWidth;
+    float difference;
+    public float heightPanel = 150f;
     private void Start()
     {
+        
+        difference = maxWidth - minWidth;
+        fullWidth = difference * 3;
         UpdateCounter();
+        StartCoroutine(DelayResettedTextPos());
+        
     }
 
     public void UpdateCounter()
@@ -38,5 +53,21 @@ public class Counter : MonoBehaviour
         text1.text = "Red Player: " + s1Percent.ToString("F2") + "%";
         text2.text = "Blue Player: " + s2Percent.ToString("F2") + "%";
         text3.text = "Yellow Player: " + s3Percent.ToString("F2") + "%";
+
+        //calculate with of panel that the width doesnt fall below the minWidth(250px) the three dont exeed the maxwidth+250*3
+        panel1.sizeDelta = new Vector2(((fullWidth / 100) * s1Percent) + minWidth, heightPanel);
+        panel2.sizeDelta = new Vector2(((fullWidth / 100) * s2Percent) + minWidth, heightPanel);
+        panel3.sizeDelta = new Vector2(((fullWidth / 100) * s3Percent) + minWidth, heightPanel);
+
+        
+    }
+
+    IEnumerator DelayResettedTextPos()
+    {
+        yield return new WaitForSeconds(0.1f);
+        text1.GetComponent<RectTransform>().localPosition = Vector3.zero;
+        text2.GetComponent<RectTransform>().localPosition = Vector3.zero;
+        text3.GetComponent<RectTransform>().localPosition = Vector3.zero;
+        yield return null;
     }
 }
